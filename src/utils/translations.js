13f62465fe5e78f7e305880b86122c5501af6bc8
limweikian0505 <limweikian@1utar.my>
2026-03-06 04:32:42 +0800

    Create translations.js for English and Chinese translations.

---
+++ src/utils/translations.js
@@ -0,0 +1,16 @@
+const translations = {
+    en: {
+        welcome: "Welcome to Piggy Accounting",
+        expense: "Expense",
+        income: "Income",
+        summary: "Summary",
+    },
+    zh: {
+        welcome: "欢迎来到Piggy Accounting",
+        expense: "开支",
+        income: "收入",
+        summary: "总结",
+    }
+};
+
+export default translations;
